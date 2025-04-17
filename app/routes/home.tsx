b2e200a3b17f3eeba56f9ebb564ac67d1ecbe563
Hector <hectorpena6590@gmail.com>
2025-04-17 16:39:30 -0700

    finshing landing page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,1 +1,15 @@
 //TODO2: please have a look at the routes/welcome.tsx file and do something similar here.
+
+import type { Route } from "./+types/home";
+import { Home as HomePage } from "../pages/home/home";
+
+export function meta({}: Route.MetaArgs) {
+    return [
+        { title: "America's most awared online library platform" },
+        { name: "description", content: "Find your favorite book with Library" },
+    ]
+}
+
+export default function Home() {
+  return <HomePage />;
+}

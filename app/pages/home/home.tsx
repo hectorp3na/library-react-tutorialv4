b9e307473f4ecee013fef1b7ba6f606af01d71dc
Hector <hectorpena6590@gmail.com>
2025-04-18 23:43:18 -0700

    fixing fontawesome

--- app/pages/home/home.tsx
+++ app/pages/home/home.tsx
@@ -2,7 +2,8 @@ import { useState } from "react";
 import LibrarySvg from "./public/assets/Library.svg";
 import UndrawBooksSvg from "./public/assets/Undraw_Books.svg";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faShoppingCart, faXmark,  } from '@fortawesome/free-solid-svg-icons';
+import { faShoppingCart, faXmark, faBars } from '@fortawesome/free-solid-svg-icons';
+
 
 export function Home() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -28,11 +29,11 @@ export function Home() {
                 </a>
               </li>
               <li className="nav__icon">
-                <a href="/cart" className="nav__link">
+                <i className="nav__link">
                   <FontAwesomeIcon icon={faShoppingCart} />
-                </a>
+                </i>
               </li>
-
+              </ul>
               <button className="btn__menu" onClick={openMenu}>
                 <svg
                   aria-hidden="true"
@@ -52,25 +53,11 @@ export function Home() {
               </button>
 
               <li className="nav__icon">
-                <a className="nav__link" href=".cart">
-                  <svg
-                    aria-hidden="true"
-                    focusable="false"
-                    data-prefix="fas"
-                    data-icon="shopping-cart"
-                    className="svg-inline--fa fa-shopping-cart fa-w-18 "
-                    role="img"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 576 512"
-                  >
-                    <path
-                      fill="currentColor"
-                      d="M528.12 301.319l47.273-208C578.806 78.301 567.391 64 551.99 64H159.208l-9.166-44.81C147.758 8.021 137.93 0 126.529 0H24C10.745 0 0 10.745 0 24v16c0 13.255 10.745 24 24 24h69.883l70.248 343.435C147.325 417.1 136 435.222 136 456c0 30.928 25.072 56 56 56s56-25.072 56-56c0-15.674-6.447-29.835-16.824-40h209.647C430.447 426.165 424 440.326 424 456c0 30.928 25.072 56 56 56s56-25.072 56-56c0-22.172-12.888-41.332-31.579-50.405l5.517-24.276c3.413-15.018-8.002-29.319-23.403-29.319H218.117l-6.545-32h293.145c11.206 0 20.92-7.754 23.403-18.681z"
-                    ></path>
-                  </svg>
+                <a href="/cart" className="nav__link">
+                  <FontAwesomeIcon icon={faShoppingCart} />
                 </a>
               </li>
-            </ul>
+           
 
             <div className="menu__backdrop">
               <button
@@ -94,7 +81,7 @@ export function Home() {
                     Books
                   </a>
                 </li>
-                <li className="menu_list">
+                <li className="menu__list">
                   <a className="menu__link" href="/cart" onClick={closeMenu}>
                     Cart
                   </a>
@@ -183,7 +170,7 @@ export function Home() {
                 </div>
 
                 <div className="highlight">
-                  <div className="highlight__img>">
+                  <div className="highlight__img">
                     <i className="fas fa-book-open feature__icon">
                     <FontAwesomeIcon icon={["fas", "book-open"]} />
                     </i>
@@ -302,7 +289,7 @@ export function Home() {
           <div className="container">
             <div className="row">
               <h2 className="section__title">
-                Discount <span className="purple">Books</span>
+                Books on  <span className="purple">Sale</span>
               </h2>
               <div className="books">
                 <div className="book">

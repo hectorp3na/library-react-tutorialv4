9a87826390e1d1fe247c8c5acf3f5c1429bab63a
Katarzyna Kowalska <k.a.kowalska@outlook.com>
2025-04-24 10:34:41 +0700

    added TODO

--- app/pages/App.js
+++ app/pages/App.js
@@ -1,23 +1,23 @@
-import "./app.css";
-import Footer from "./components/Footer";
-import Nav from "./components/Nav";
-import Home from "./pages/Home";
-import Books from "./pages/Books";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+//TODO: please delte this file - again your routes are defined else where - please refer to previous messages
 
+// import "./app.css";
+// import Footer from "./components/Footer";
+// import Nav from "./components/Nav";
+// import Home from "./pages/Home";
+// import Books from "./pages/Books";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
 
-
-function App() {
-    return (
-<Router>
-    <div className="App">
-        <Nav />
-        <Route path="/" exact Component={Home} />
-        <Route path="/books" Component={Books} />
-        <Home />
-        <Footer />
-    </div>
-</Router>
-    )
-}
-export default App;
+// function App() {
+//   return (
+//     <Router>
+//       <div className="App">
+//         <Nav />
+//         <Route path="/" exact Component={Home} />
+//         <Route path="/books" Component={Books} />
+//         <Home />
+//         <Footer />
+//       </div>
+//     </Router>
+//   );
+// }
+// export default App;
